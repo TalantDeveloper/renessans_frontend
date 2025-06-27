feb9fccbc3140562ca6d56831810ed3b037a364e
Botirjon Ergashov <139768692+TalantDeveloper@users.noreply.github.com>
2025-06-27 08:45:04 +0500

    Update BaseData.js

--- src/user/home/BaseData.js
+++ src/user/home/BaseData.js
@@ -2,3 +2,8 @@
 export const BaseURL = "https://sayt.renessans-edu.uz/";
 export const LastSite = "https://renessans-edu.uz";
 export const testUrl = "http://10.10.1.24:8000";
+
+
+export const univercity = [
+    {"text": "salom", "path": "/s"}
+]
